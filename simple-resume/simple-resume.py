7e288e7f75d73bae0230fb45b6c6bb5bc3e44c46
Abhinav Sharma <abhi18av@gmail.com>
2018-12-02 19:47:16 +0530

    as of now, something is broken

--- simple-resume/simple-resume.py
+++ simple-resume/simple-resume.py
@@ -1,6 +1,6 @@
-from pylatex import Document, Section, Subsection, Command, UnsafeCommand
 from pylatex.base_classes import Environment, CommandBase, Arguments
 from pylatex.package import Package
+from pylatex import Document, Section, Subsection, Command, UnsafeCommand
 from pylatex.utils import NoEscape, italic
 
 
@@ -27,156 +27,48 @@ class ExampleEnvironment(Environment):
                 Package('sectsty')]
 
 
-# class ExampleCommand(CommandBase):
-#     """
-#     A class representing a custom LaTeX command.
-#     This class represents a custom LaTeX command named
-#     ``exampleCommand``.
-#     """
 
-#     _latex_name = 'exampleCommand'
-#     packages = [Package('graphicx')]
+#######################################
 
-#\sepspace
-class SepSpace(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-#\newcommand{\sepspace}{\vspace*{1em}}
-    _latex_name = 'sepspace'
-#    packages = [('graphicx')]
-
-#\MyName
 class MyName(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-# { % Name
-# 		\Huge \usefont{OT1}{phv}{b}{n} \hfill #1
-# 		\par \normalsize \normalfont}
-
-
     _latex_name = 'MyName'
-#   packages = [Package('graphicx')]
 
-
-#\MySlogan
 class MySlogan(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-# \newcommand{\MySlogan}[1]{ % Slogan (optional)
-# 		\large \usefont{OT1}{phv}{m}{n}\hfill \textit{#1}
-# 		\par \normalsize \normalfont}
-
-
     _latex_name = 'MySlogan'
-#    packages = [Package('graphicx')]
 
-
-#\NewPart
 class NewPart(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-
     _latex_name = 'NewPart'
-#    packages = [Package('graphicx')]
 
-
-#\PersonalEntry
 class PersonalEntry(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-# \newcommand{\PersonalEntry}[2]{
-# 		\noindent\hangindent=2em\hangafter=0 % Indentation
-# 		\parbox{\spacebox}{        % Box to align text
-# 		\textit{#1}}		       % Entry name (birth, address, etc.)
-# 		\hspace{1.5em} #2 \par}    % Entry value
-
     _latex_name = 'PersonalEntry'
-#    packages = [Package('graphicx')]
 
-
-#\SkillsEntry
 class SkillsEntry(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-#       \newcommand{\SkillsEntry}[2]{      % Same as \PersonalEntry
-# 		\noindent\hangindent=2em\hangafter=0 % Indentation
-# 		\parbox{\spacebox}{        % Box to align text
-# 		\textit{#1}}			   % Entry name (birth, address, etc.)
-# 		\hspace{1.5em} #2 \par}    % Entry value
-
-
     _latex_name = 'SkillsEntry'
-#    packages = [Package('graphicx')]
 
-
-#\EducationEntry
 class EducationEntry(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-
     _latex_name = 'EducationEntry'
-#    packages = [Package('graphicx')]
 
-
-#\WorkEntry
 class WorkEntry(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-
     _latex_name = 'WorkEntry'
-#    packages = [Package('graphicx')]
 
 
-
-
-#\PersonalEntry
-class PersonalEntry(CommandBase):
-    """
-    A class representing a custom LaTeX command.
-    This class represents a custom LaTeX command named
-    ``exampleCommand``.
-    """
-
-    _latex_name = 'PersonalEntry'
-#    packages = [Package('graphicx')]
+#######################################
 
 
 
 
 
+if __name__ == '__main__':
 
 
+    doc = Document(documentclass = 'scrartcl' , document_options = ["paper=a4","fontsize=11pt"])
 
-def fill_document(doc):
-    """Add a section, a subsection and some text to the document.
 
-    :param doc: the document
-    :type doc: :class:`pylatex.document.Document` instance
-    """
+    doc.preamble.append(Command('title', 'Resumee'))
+    doc.preamble.append(Command('author', 'Abhinav Sharma'))
+    doc.preamble.append(Command('date', NoEscape(r'\today')))
+    doc.append(NoEscape(r'\maketitle'))
+
     with doc.create(Section('Experience')):
         doc.append('Cyber Indian App Stores Pvt. Ltd.')
         doc.append(italic('Chief Technology Officer'))
@@ -185,65 +77,49 @@ def fill_document(doc):
             doc.append('Software Engineer AI/ML')
 
 
-if __name__ == '__main__':
-    # Basic document
-    #doc = Document('simple')
-    #fill_document(doc)
+###########################
 
-#    doc.generate_pdf(clean_tex=False)
-#   doc.generate_tex()
-
-    # Document with `\maketitle` command activated
-    #doc = Document()
-    doc = Document(documentclass = 'scrartcl' , document_options = ["paper=a4","fontsize=11pt"])
-
-    doc.preamble.append(Command('title', 'Resumee'))
-    doc.preamble.append(Command('author', 'Abhinav Sharma'))
-    doc.preamble.append(Command('date', NoEscape(r'\today')))
-    doc.append(NoEscape(r'\maketitle'))
-
-    fill_document(doc)
-
-   # doc.generate_pdf('basic_maketitle', clean_tex=False)
+###########################
 
 
-   # # Define the new command
-   #  new_comm = UnsafeCommand('newcommand', '\exampleCommand', options=3,
-   #                           extra_arguments=r'\color{#1} #2 #3 \color{black}')
-   #  doc.append(new_comm)
 
-############ MACROS
-
-#\newcommand{\sepspace}{\vspace*{1em}}
     sepspace = UnsafeCommand('newcommand', r'\sepspace', options=0,
                              extra_arguments=r"""
                              \vspace*{1em}
-
 """)
-    doc.append(sepspace)
 
-# { % Name
-# 		\Huge \usefont{OT1}{phv}{b}{n} \hfill #1
-# 		\par \normalsize \normalfont}
+#    doc.append(sepspace)
+    doc.append(sepspace(arguments=Arguments())
+
+
+###########################
+
+###########################
+
 
     MyName = UnsafeCommand('newcommand', r'\MyName', options=1,
                              extra_arguments=r"""
     		\Huge \usefont{OT1}{phv}{b}{n} \hfill #1
      		\par \normalsize \normalfont
-
 """)
 
     doc.append(MyName)
 
-# \newcommand{\MySlogan}[1]{ % Slogan (optional)
-# 		\large \usefont{OT1}{phv}{m}{n}\hfill \textit{#1}
-# 		\par \normalsize \normalfont}
+
+    doc.append(MyName(arguments=Arguments("Abhinav Sharma")))
+
+
+
+###########################
+
+###########################
+
+
+
     MySlogan = UnsafeCommand('newcommand', r'\MySlogan', options=1,
                              extra_arguments=r"""
   		\large \usefont{OT1}{phv}{m}{n}\hfill \textit{#1}
 		\par \normalsize \normalfont
-
-
 """)
 
 
@@ -252,15 +128,11 @@ if __name__ == '__main__':
 
 
     NewPart = UnsafeCommand('newcommand', r'\NewPart', options=1,
-                             extra_arguments=r"""\section*{\uppercase{#1}}""")
+                             extra_arguments=r"""
+                             \section*{\uppercase{#1}}
+""")
 
     doc.append(NewPart)
-
-# \newcommand{\PersonalEntry}[2]{
-# 		\noindent\hangindent=2em\hangafter=0 % Indentation
-# 		\parbox{\spacebox}{        % Box to align text
-# 		\textit{#1}}		       % Entry name (birth, address, etc.)
-# 		\hspace{1.5em} #2 \par}    % Entry value
 
 
     PersonalEntry = UnsafeCommand('newcommand', r'\PersonalEntry', options=2,
@@ -269,16 +141,10 @@ if __name__ == '__main__':
 		\parbox{\spacebox}{        % Box to align text
 		\textit{#1}}		       % Entry name (birth, address, etc.)
 		\hspace{1.5em} #2 \par}    % Entry value
-
  """)
 
     doc.append(PersonalEntry)
 
-#       \newcommand{\SkillsEntry}[2]{      % Same as \PersonalEntry
-# 		\noindent\hangindent=2em\hangafter=0 % Indentation
-# 		\parbox{\spacebox}{        % Box to align text
-# 		\textit{#1}}			   % Entry name (birth, address, etc.)
-# 		\hspace{1.5em} #2 \par}    % Entry value
 
     SkillsEntry = UnsafeCommand('newcommand', r'\SkillsEntry', options=2,
                              extra_arguments=r"""
@@ -287,7 +153,6 @@ if __name__ == '__main__':
 		\parbox{\spacebox}{        % Box to align text
 		\textit{#1}}			   % Entry name (birth, address, etc.)
 		\hspace{1.5em} #2 \par    % Entry value
-
 """)
 
     doc.append(SkillsEntry)
@@ -301,13 +166,9 @@ if __name__ == '__main__':
 		\noindent \textit{#3} \par        % School
 		\noindent\hangindent=2em\hangafter=0 \small #4 % Description
 		\normalsize \par
-
  """)
 
     doc.append(EducationEntry)
-
-
-
 
 
     WorkEntry = UnsafeCommand('newcommand', r'\WorkEntry', options=2,
@@ -328,8 +189,6 @@ if __name__ == '__main__':
 
     # # Use our newly created command with different arguments
     # doc.append(ExampleCommand(arguments=Arguments('blue', 'Hello', 'World!')))
-    # doc.append(ExampleCommand(arguments=Arguments('green', 'Hello', 'World!')))
-    # doc.append(ExampleCommand(arguments=Arguments('red', 'Hello', 'World!')))
 
 
 
