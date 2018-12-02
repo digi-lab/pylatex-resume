341cbb06b0026cc2f415c0e147d52af4e2350551
Abhinav Sharma <abhi18av@gmail.com>
2018-12-02 16:37:04 +0530

    making progress with the automatic imports and commands

--- simple-resume/simple-resume.py
+++ simple-resume/simple-resume.py
@@ -1,5 +1,155 @@
-from pylatex import Document, Section, Subsection, Command
-from pylatex.utils import italic, NoEscape
+from pylatex import Document, Section, Subsection, Command, UnsafeCommand
+from pylatex.base_classes import Environment, CommandBase, Arguments
+from pylatex.package import Package
+from pylatex.utils import NoEscape, italic
+
+
+class ExampleEnvironment(Environment):
+    """
+    A class representing a custom LaTeX environment.
+    This class represents a custom LaTeX environment named
+    ``exampleEnvironment``.
+    """
+
+    _latex_name = 'exampleEnvironment'
+    packages = [
+                Package('mdframed'),
+                Package('english'),
+                Package('utf8x'),
+                Package('microtype'),
+                Package('amsmath'),
+                Package('amsfonts'),
+                Package('amsthm'),
+                Package('graphicx'),
+                Package('svgnames'),
+                Package('geometry'),
+                Package('url'),
+                Package('sectsty')]
+
+
+class ExampleCommand(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'exampleCommand'
+    packages = [Package('graphicx')]
+
+#\sepspace
+class SepSpace(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'sepspace'
+#    packages = [('graphicx')]
+
+#\MyName
+class MyName(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'MyName'
+    packages = [Package('graphicx')]
+
+
+#\MySlogan
+class MySlogan(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'MySlogan'
+#    packages = [Package('graphicx')]
+
+
+#\NewPart
+class NewPart(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'NewPart'
+#    packages = [Package('graphicx')]
+
+
+#\PersonalEntry
+class PersonalEntry(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'PersonalEntry'
+#    packages = [Package('graphicx')]
+
+
+#\SkillsEntry
+class SkillsEntry(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'SkillsEntry'
+#    packages = [Package('graphicx')]
+
+
+#\EducationEntry
+class EducationEntry(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'EducationEntry'
+#    packages = [Package('graphicx')]
+
+
+#\WorkEntry
+class WorkEntry(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'WorkEntry'
+#    packages = [Package('graphicx')]
+
+
+
+
+#\PersonalEntry
+class PersonalEntry(CommandBase):
+    """
+    A class representing a custom LaTeX command.
+    This class represents a custom LaTeX command named
+    ``exampleCommand``.
+    """
+
+    _latex_name = 'PersonalEntry'
+#    packages = [Package('graphicx')]
+
+
+
+
+
+
 
 
 def fill_document(doc):
@@ -34,13 +184,51 @@ if __name__ == '__main__':
 
     fill_document(doc)
 
-#    doc.generate_pdf('basic_maketitle', clean_tex=False)
+   # doc.generate_pdf('basic_maketitle', clean_tex=False)
+
+
+   # Define the new command
+    new_comm = UnsafeCommand('newcommand', '\exampleCommand', options=3,
+                             extra_arguments=r'\color{#1} #2 #3 \color{black}')
+    doc.append(new_comm)
+
+    # Use our newly created command with different arguments
+    doc.append(ExampleCommand(arguments=Arguments('blue', 'Hello', 'World!')))
+    doc.append(ExampleCommand(arguments=Arguments('green', 'Hello', 'World!')))
+    doc.append(ExampleCommand(arguments=Arguments('red', 'Hello', 'World!')))
+
+
+
 
     # Add stuff to the document
     with doc.create(Section('Experience')):
         doc.append('Some text.')
 
+
+    # Define a style for our box
+    mdf_style_definition = UnsafeCommand('mdfdefinestyle',
+                                         arguments=['my_style',
+                                                    ('linecolor=#1,'
+                                                     'linewidth=#2,'
+                                                     'leftmargin=1cm,'
+                                                     'leftmargin=1cm')])
+
+
+
+    # Define the new environment using the style definition above
+    new_env = UnsafeCommand('newenvironment', 'exampleEnvironment', options=2,
+                            extra_arguments=[
+                                mdf_style_definition.dumps() +
+                                r'\begin{mdframed}[style=my_style]',
+                                r'\end{mdframed}'])
+    doc.append(new_env)
+
+    # Usage of the newly created environment
+    with doc.create(
+            ExampleEnvironment(arguments=Arguments('red', 3))) as environment:
+        environment.append('This is the actual content')
+
+
 #    doc.generate_pdf('basic_maketitle2', clean_tex=False)
     tex = doc.dumps()  # The document as string in LaTeX syntax
-    doc.generate_tex("./abhi18av")
-
+    doc.generate_tex("./simple-resume")
